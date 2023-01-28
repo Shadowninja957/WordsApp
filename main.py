a9289e8a3ffd480f9dad70a3800f473d7568be48
Dharian Malchan <dharianmalchan@gmail.com>
2023-01-28 11:37:50 -0400

    added all the elements

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from Categories import *
 import tkinter as tk
+
+# categories in a list use the index in listbox to identify the object categories in this list
+categories = [Anatomy(),People(),Animal(),Place(),Thing(),Movie(),TvShow(),Anime(),Cartoon(),Music(),Art(),Verb(),Adjective(),Food()
+,Drink(),CustomWord(),LeagueOfLegends(),Apex(),Overwatch(),Color(),Astrology()]
 
 root = tk.Tk()
 
 canvas = tk.Canvas(root, width=600, height=400)
-canvas.grid(columnspan=3, rowspan=5)
+canvas.grid(columnspan=3, rowspan=6)
 
 '''TOP Labels'''
 appName = tk.Label(root, text="Words", font="Raleway")
@@ -13,28 +18,16 @@ instructions = tk.Label(root, text="Generate a .txt file with 25 words or 400 wo
 instructions.grid(column=1, row=1)
 '''TOP Labels'''
 
+# Generate Words
 '''Category Select Listbox'''
 listbox_frame = tk.Frame(root)
 scrollbar = tk.Scrollbar(listbox_frame, orient="vertical")
 
 categoryBox = tk.Listbox(listbox_frame, selectmode="multiple", yscrollcommand=scrollbar.set)
-categoryBox.insert(1, "Python")
-categoryBox.insert(2, "Perl")
-categoryBox.insert(3, "C")
-categoryBox.insert(4, "PHP")
-categoryBox.insert(5, "JSP")
-categoryBox.insert(6, "Ruby")
-categoryBox.insert(7, "Python")
-categoryBox.insert(8, "Perl")
-categoryBox.insert(9, "C")
-categoryBox.insert(10, "PHP")
-categoryBox.insert(11, "JSP")
-categoryBox.insert(12, "Ruby")
 
 scrollbar.config(command=categoryBox.yview())
 scrollbar.pack(side="right", fill="y")
 categoryBox.pack()
-listbox_frame.grid(column=1, row=2)
 '''Category Select '''
 
 '''Generator Words Button '''
@@ -64,5 +57,43 @@ generateWords2_btn.pack(side="right")
 genButtonsFrame.grid(column=1, row=3)
 '''Generator Words Button '''
 
+# Add words
+'''Add words listbox'''
+listbox_frame2 = tk.Frame(root)
+scrollbar2 = tk.Scrollbar(listbox_frame2, orient="vertical")
+
+categoryBox2 = tk.Listbox(listbox_frame2, selectmode="single", yscrollcommand=scrollbar2.set)
+for item in categories:
+    categoryBox.insert(categories.index(item), item.getCategory())
+    categoryBox2.insert(categories.index(item), item.getCategory())
+
+scrollbar2.config(command=categoryBox2.yview())
+scrollbar2.pack(side="right", fill="y")
+categoryBox2.pack()
+
+listbox_frame.grid(column=1, row=2)
+listbox_frame2.grid(column=1, row=4)
+'''Add words listbox'''
+
+'''Add words textfield'''
+entryFrame = tk.Frame(root)
+
+entryLabel = tk.Label(entryFrame, text="Enter words")
+entryLabel.pack(side="top")
+
+entry = tk.Entry(entryFrame, bd=5)
+entry.pack(side="bottom")
+
+entryFrame.grid(column=1, row=5)
+'''Add words textfield'''
+
+'''Add words button'''
+def addWords():
+    print("Added Words!")
+
+addWordsbtn = tk.Button(root, text="Add Words", command=addWords)
+addWordsbtn.grid(column=1, row=6)
+
+'''Add words button'''
 
 root.mainloop()
